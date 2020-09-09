665e0fb6803507eee0fbcb139453c7e3072be9de
Abubakar Dongula <adongula@gmail.com>
2020-09-09 22:06:14 +0400

    Updated API Key and network name

--- Python/Networking/Meraki/merakidemo.py
+++ Python/Networking/Meraki/merakidemo.py
@@ -4,20 +4,12 @@ import json
 url = "https://dashboard.meraki.com/api/v0/organizations"
 
 headers = {
-    'X-Cisco-Meraki-API-Key': "6bec40cf957de430a6f1f2baa056b99a4fac9ea0",
-    'User-Agent': "PostmanRuntime/7.16.3",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "67a3f4c9-bcb4-43a5-bcde-c05ec3e976af,b27f7dd9-3ebc-4d17-a0a8-d62ab5cafb99",
-    'Accept-Encoding': "gzip, deflate",
-    'Referer': "https://api.meraki.com/api/v0/organizations",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
+    'X-Cisco-Meraki-API-Key': "9103a4d27fc670d50f63f9d6047c0de121f475f1",
 }
 
 response = requests.get(url, headers=headers).json()
 
-#print(json.dumps(response, indent=2, sort_keys=True))
+# print(json.dumps(response, indent=2, sort_keys=True))
 
 for response_org in response:
     if response_org['name'] == 'DevNet Sandbox':
@@ -30,7 +22,7 @@ net_url = f'{url}/{orgId}/networks'
 networks = requests.get(net_url, headers=headers).json()
 print(json.dumps(networks, indent=2, sort_keys=True))
 for network in networks:
-    if network['name'] == 'DNSMB2':
+    if network['name'] == 'DNSMB3-axxxxxagmail.com':
         netId = network['id']
 
 
